a130accd1fdc82cd5ee9e352f83dc3fe6be62844
Bartosz Szostak <bartosz.szostak@xware.pl>
2024-12-20 12:16:29 +0100

    Initial revision of the xHello application

---
+++ server/main.cpp
@@ -0,0 +1,17 @@
+#include <rfl/json.hpp>
+#include <spdlog/spdlog.h>
+
+struct Person {
+  std::string first_name{};
+  std::string last_name{};
+  int age{};
+};
+
+int main() {
+  spdlog::info("Hello from xHello server");
+
+  const auto homer = Person{"Homer", "Simpson", 45};
+  rfl::json::write(homer, std::cout) << std::endl;
+
+  return EXIT_SUCCESS;
+}
